2c436b4d7c469cb7628edd8cb78a576409810600
Rachel Isaac <rachelisaac995@gmail.com>
2023-04-10 19:44:00 +0400

    Fix Linter errors

--- Person.rb
+++ Person.rb
@@ -1,33 +1,21 @@
-require_relative 'student'
-require_relative 'teacher'
+class Person
+  attr_reader :id
+  attr_accessor :name, :age, :parent_permission
 
-class Person 
+  def initialize(age:, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    attr_reader :id, :name, :age, :parent_permission
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
 
-    def initialize(name = "Unknown", age, parent_permission = true)
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  private
 
-    def name=(name)
-        @name = name
-    end
-
-    def age=(age)
-        @age = age
-    end
-
-    def can_use_services?
-        is_of_age? || @parent_permission
-    end
-
-    private
-
-    def is_of_age? 
-        @age >= 18
-    end
-
+  def of_age?
+    @age >= 18
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,13 +1,14 @@
-require_relative 'Person'
+require './person'
 
 class Teacher < Person
-    attr_accessor :specialization
+  attr_accessor :specialization
 
-    def initialize(id, name = "Unknown", age, parent_permission = true classroom, specialization)
-        super(name, age, parent_permission)
-        @specialization = specialization
-    end
-    def can_use_services?
-        true
-    end
+  def initialize(age:, specialization:, name: 'Unknown', parent_permission: true)
+    super(name, age, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
 end
