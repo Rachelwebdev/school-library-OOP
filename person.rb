3de5ca5445a3067e4371c3526411112017b298e8
Rachel Isaac <rachelisaac995@gmail.com>
2023-04-12 16:44:42 +0400

    Add many-to-many relationship btw Person and Book

--- person.rb
+++ person.rb
@@ -1,13 +1,14 @@
 require_relative 'nameable'
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age, :parent_permission
+  attr_accessor :name, :age, :parent_permission, :rentals
 
   def initialize(age:, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
     super()
   end
 
--- rental.rb
+++ rental.rb
@@ -1,7 +1,13 @@
 class Rental
-    attr_accessor :date
+  attr_accessor :date, :book, :person
 
-    def initialize(date:)
-        @date = date
-    end
+  def initialize(date:, book:, person:)
+    @date = date
+    @book = book
+    @person = person
+
+    book.rentals << self
+
+    person.rentals << self
+  end
 end
