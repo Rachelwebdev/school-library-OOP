f6f9c900b749165bef53b21d6eebf8da9ed57a8d
Rachel Isaac <rachelisaac995@gmail.com>
2023-04-14 14:05:18 +0400

    Add person for teacher or student

--- app.rb
+++ app.rb
@@ -32,4 +32,43 @@ class App
             end
         end
     end
+
+    def create_person 
+        person_type = person_type_switch
+        return if person_type.nil?
+
+        print 'Name: '
+        name = gets.chomp
+        print 'Age: '
+        age = gets.chomp.to_i
+        person = 
+        case person_type
+        when '1'
+            print 'classroom: '
+            classroom = gets.chomp
+            print 'Has parent permission? [Y/N]: '
+            parent_permission = gets.chomp
+            parent_permission = parent_permission.downcase == 'y'
+            classroom = Classsroom.new(classroom)
+            Student.new(classroom, age, name, parent_permission: parent_permission)
+        when '2'
+            print 'Specialization: '
+            Specialization = gets.chomp
+
+            Teacher.new(age, specialization, name)
+        end
+
+        @people << person
+        puts 'Person created successfully'
+    end
+
+    def person_type_switch
+        print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+        person_type = gets.chomp
+        if person_type != '1' && person_type ! = '2'
+            puts 'Invalid option'
+            return
+        end
+        person_type
+    end
 end
