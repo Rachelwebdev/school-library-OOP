0fd50f53c79fd054e07f2fbd85c2f0e9f8b5139d
Rachel Isaac <rachelisaac995@gmail.com>
2023-04-14 14:21:30 +0400

    Allows users choose an option

--- main.rb
+++ main.rb
@@ -23,5 +23,26 @@ class Main
     end
     gets.chomp.to_i
   end
-   
+    res = App.new
+  loop do
+    case menu
+    when 1
+      res.all_books
+    when 2
+      res.all_people
+    when 3
+      res.create_person
+    when 4
+      res.create_book
+    when 5
+      res.create_rental
+    when 6
+      res.list_rentals_of_person
+    when 7
+      puts 'Thank you for using this app!'
+      exit
+    else
+      puts 'Please choose a number between 1 and 7'
+    end
+  end
 end
